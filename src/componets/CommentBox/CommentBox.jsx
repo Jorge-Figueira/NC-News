0105fcfd54224048a68d83edfc8681a9a86dffa6
Jorge-Figueira <xurxo.figueira@gmail.com>
2022-03-18 19:48:59 +0000

    removes the comment section when no users is logged

--- src/componets/CommentBox/CommentBox.jsx
+++ src/componets/CommentBox/CommentBox.jsx
@@ -13,64 +13,69 @@ export default function CommentBox({ article_id }) {
 
   useEffect(() => {}, [params, sent]);
   return (
-    <section className="comment_section">
-      {user !== "" && !params && (
-        <form
-          className="comment_stage"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (comment !== "") {
-              return setParams({ username: user, body: comment });
-            } else {
-              return false;
-            }
-          }}
-        >
-          <label> Write a comment</label>
+    <>
+      {user === "" && null}
+      {user !== "" && (
+        <section className="comment_section">
+          {!params && (
+            <form
+              className="comment_stage"
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (comment !== "") {
+                  return setParams({ username: user, body: comment });
+                } else {
+                  return false;
+                }
+              }}
+            >
+              <label> Write a comment</label>
 
-          <textarea
-            cols="40"
-            rows="5"
-            value={comment}
-            onChange={(e) => {
-              e.preventDefault();
-              setComment(e.target.value);
-            }}
-          ></textarea>
-          <button type="submit">Preview</button>
-          <button
-            type="button"
-            onClick={() => {
-              setComment("");
-            }}
-          >
-            Delete
-          </button>
-        </form>
-      )}
-      {user !== "" && params && sent === false && (
-        <section className="comment_stage">
-          <h3>Are you sure that you want to submit?</h3>
-          <article>{params.body}</article>
-          <button
-            type="submit"
-            onClick={() => {
-              api.postComment(params, article_id);
-              setSent(true);
-            }}
-          >
-            Submit
-          </button>
-          <button
-            onClick={() => {
-              setParams(false);
-            }}
-          >
-            Edit
-          </button>
+              <textarea
+                cols="40"
+                rows="5"
+                value={comment}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setComment(e.target.value);
+                }}
+              ></textarea>
+              <button type="submit">Preview</button>
+              <button
+                type="button"
+                onClick={() => {
+                  setComment("");
+                }}
+              >
+                Delete
+              </button>
+            </form>
+          )}
+          {params && sent === false && (
+            <section className="comment_stage">
+              <h3>Are you sure that you want to submit?</h3>
+              <article>{params.body}</article>
+              <button
+                type="submit"
+                onClick={() => {
+                  api.postComment(params, article_id);
+                  setSent(true);
+                }}
+              >
+                Submit
+              </button>
+              <button
+                onClick={() => {
+                  setParams(false);
+                }}
+              >
+                Edit
+              </button>
+            </section>
+          )}
+          {sent === true && <p className="comment_stage">Comment Sent</p>}
         </section>
       )}
-      {sent === true && <p className="comment_stage">Comment Sent</p>}
-    </section>
+    </>
   );
 }
